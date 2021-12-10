327b518b7b31ae820eaf53c70cb5c0524488d5bb
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2021-12-09 20:21:20 -0800

    CDK initial stuff

---
+++ good-coffee/src/main/java/org/edwork/goodcoffee/GoodCoffeeStack.java
@@ -0,0 +1,24 @@
+package org.edwork.goodcoffee;
+
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+// import software.amazon.awscdk.Duration;
+// import software.amazon.awscdk.services.sqs.Queue;
+
+public class GoodCoffeeStack extends Stack {
+    public GoodCoffeeStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public GoodCoffeeStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        // The code that defines your stack goes here
+
+        // example resource
+        // final Queue queue = Queue.Builder.create(this, "GoodCoffeeQueue")
+        //         .visibilityTimeout(Duration.seconds(300))
+        //         .build();
+    }
+}
